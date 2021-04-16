cbd021e0353f9edf0401946890c97d1d8afc9993
VictorOliveira02 <victoralvees17@gmail.com>
2021-04-16 15:57:30 -0300

    initial commit

---
+++ testesCovid.c
@@ -0,0 +1,21 @@
+/*
+                                        Universidade Federal de Viçosa - CRP
+                                                13/05/2021
+
+    -> SIN110 - Prof. Matheus Haddad e Prof. Leandro Furtado
+    -> Trabalho Final - testesCovid
+    -> Reposiório do projeto: https://github.com/VictorOliveira02/Gerenciador-TestesCovid
+
+*Victor Alves de Oliveira - 6954 - T1
+*Rafael - 1234 - T1
+*Bruno- 4321 - T1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    return 0;
+}
